fd59783b8bcf82b1921a713a828cf97fe47753e2
Ayush632 <35390743+Ayush632@users.noreply.github.com>
2019-09-22 14:57:36 +0530

    wallet implemented

--- main.py
+++ main.py
@@ -13,14 +13,17 @@ import mysql.connector
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
-
+name=None
 @app.route('/')
 def login():
     return render_template('login.html')
+
+
 @app.route("/check",methods=["POST"])
 def check():
  
  # cur=conn.cursor()
+  global name
   name=request.form.get("user_name")
   #print(type(name))
   passw=request.form.get("password")
@@ -46,6 +49,8 @@ def check():
 @app.route("/sign_up")
 def sign():
     return render_template('sign.html')
+
+
 @app.route("/one",methods=["GET","POST"])
 def first():
   #  conne=sqlite3.connect('auction.sqlite')
@@ -80,7 +85,50 @@ def first():
       return render_template("login.html")
     else:
       cursor.execute("""insert into user values (%s,%s,%s,%s,%s,%s)""",(name,passw,first_name,last_name,email_id,ph_no))
+      cursor.execute("""insert into wallet values (%s,%s)""",(name,0))
       conn.commit()
       conn.close()
       return render_template("options.html")
   
+@app.route("/wallet",methods=["GET","POST"])
+def wallet():
+  conn = None
+  conn = mysql.connector.connect(host='localhost',
+                                       user='ayush',
+                                       password= '9290827021')
+  cursor=conn.cursor()
+  cursor.execute("use prj")
+  print(name)
+  cursor.execute("""select * from wallet where user_name = %s""",(name,))
+  row=cursor.fetchall()
+  print(row)
+  for i in row:
+    print(i)
+    cr=i[1]
+    print(cr)
+  conn.close()
+  return render_template("wallet.html",cr=cr)
+
+@app.route("/addcredit",methods=["GET","POST"])
+def addcredit():
+  toadd=request.form.get("adcredits")
+  conn = None
+  conn = mysql.connector.connect(host='localhost',
+                                       user='ayush',
+                                       password= '9290827021')
+  cursor=conn.cursor()
+  cursor.execute("use prj")
+  cursor.execute("""select * from wallet where user_name = %s""",(name,))
+  row=cursor.fetchall()
+  print(row)
+  for i in row:
+    print(i)
+    cr=i[1]
+  toaddin=(int)(toadd)
+  cr=cr+toaddin
+  cursor.execute("""update wallet set credits=%s where user_name = %s""",(cr,name,))
+  conn.commit()
+  conn.close()
+  return render_template("wallet.html",cr=cr)
+
+
